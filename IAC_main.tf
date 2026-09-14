fa35d4fbd9a6b4ea96ddff38dc37958109efd32e
nithish23rr-art <nithish23rr@gmail.com>
2026-09-14 23:22:48 +0530

    Set count for aws_instance to 1

--- IAC_main.tf
+++ IAC_main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "web_server" {
   ami           = "ami-04ccd752e0917a978"
   instance_type = "t2.micro"
-  count         = 
+  count         = 1
 
 
  tags = {
